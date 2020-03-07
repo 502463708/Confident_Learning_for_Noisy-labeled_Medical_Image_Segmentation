2261800b4bc0eac2293d90a531ceda586e446ba2
hkjcpy <115010133@link.cuhk.edu.cn>
2020-03-07 16:28:02 +0800

    add gaussian noise function

--- utils/noisy_dataset_generation.py
+++ utils/noisy_dataset_generation.py
@@ -62,7 +62,8 @@ def foreground_move(src_label_np, max_distance):
 
     height, width = src_label_np.shape
 
-    padded_label_np = np.pad(src_label_np, ((max_distance, max_distance), (max_distance, max_distance)), 'constant', constant_values=0)
+    padded_label_np = np.pad(src_label_np, ((max_distance, max_distance), (max_distance, max_distance)), 'constant',
+                             constant_values=0)
 
     r = np.random.randint(0, 2 * max_distance)
     c = np.random.randint(0, 2 * max_distance)
@@ -72,6 +73,41 @@ def foreground_move(src_label_np, max_distance):
     assert src_label_np.shape == dst_label_np.shape
 
     return dst_label_np
+
+
+def gaussian_noise(src_label_np, max_radius, move_ratio):
+    assert max_radius >= 0
+    assert move_ratio > 0
+    assert move_ratio < 1
+
+    height, width = src_label_np.shape
+    edges = cv2.Canny(src_label_np, threshold1=64, threshold2=200)
+
+    random_matrix = np.random.randint(100, size=(height, width))
+    random_matrix[random_matrix <= move_ratio * 100] = 0
+    random_matrix[random_matrix > 0] = 1
+    choosen_edges = edges * random_matrix
+    choose_index = np.where(choosen_edges > 0)  # tutle with length 2   row and column
+
+    dst_label_np = src_label_np
+    for index in range(len(choose_index[0])):
+        row = choose_index[0][index]
+        column = choose_index[1][index]
+        center_coordinates = (column, row)
+        radius = np.random.randint(max_radius, size=1)
+        coin = np.random.randint(2, size=1)
+        if coin == 1:
+            dst_label_np = cv2.circle(dst_label_np, center_coordinates, radius, thickness=-1, color=255)
+        else:
+            dst_label_np = cv2.circle(dst_label_np, center_coordinates, radius, thickness=-1, color=0)
+    return dst_label_np
+
+
+img=cv2.imread('/data1/minqing/data/JRST/noisy-data-alpha-0.1-beta1-10-beta2-15/all/training/clavicle/JPCLN001.png',0)
+dst=gaussian_noise(src_label_np=img, max_radius=2, move_ratio=0.25)
+cv2.imshow('image',dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
 def add_noise(src_label_np, beta1, beta2):
